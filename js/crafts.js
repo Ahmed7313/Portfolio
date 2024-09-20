69a5f87d6992d863b7a6600af272474b9b1af65c
Ahmed Rabee <35776251+Ahmed7313@users.noreply.github.com>
2024-09-20 22:37:47 +0300

    ss

--- js/crafts.js
+++ js/crafts.js
@@ -21,7 +21,7 @@ async function loadCrafts() {
         <div class="p-6">
           <h3 class="text-2xl font-semibold mb-2">${project.title}</h3>
           <p class="text-gray-700 mb-4">${project.content.substring(0, 100)}...</p>
-          <a href="project.html?id=${project.id}" class="inline-flex items-center px-4 py-2 bg-yellow-600 hover:bg-yellow-700 text-white rounded-full btn-animate">
+          <a href="Crafts/project.html?id=${project.id}" class="inline-flex items-center px-4 py-2 bg-yellow-600 hover:bg-yellow-700 text-white rounded-full btn-animate">
             <i class="fas fa-info-circle mr-2"></i> View Details
           </a>
         </div>
